4782988dca29eea9f2fc8ebbd9657edd95f7c67b
Abiguelini <114603454+Abiguelini@users.noreply.github.com>
2024-02-08 16:08:27 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/ads/course/resources/UserResources.java
+++ src/main/java/com/ads/course/resources/UserResources.java
@@ -1,22 +1,35 @@
 package com.ads.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ads.course.entities.User;
+import com.ads.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 
 public class UserResources {
 	
-	@GetMapping
-	public ResponseEntity<User> findALl() {
+	@Autowired
+	private UserService service;
 	
-		User u = new User(1l, "Maria", "Maria@gmail.com", "(51)93128312", "1234");
-		return ResponseEntity.ok().body(u);
+	@GetMapping
+	public ResponseEntity<List<User>> findALl() {
+		List<User>list=service.findall();
+		return ResponseEntity.ok().body(list);
 
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User>findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return  ResponseEntity.ok().body(obj);
+			}
 }
